1fb96bcb5076476d07668d7b902de950a64757dc
Drisanth M <135306392+Drisanth@users.noreply.github.com>
2025-04-10 12:30:21 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World from Node.js!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is a sample Node.js project.');
+});
+
+app.post('/echo', (req, res) => {
+  res.json({
+    youSent: req.body
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
